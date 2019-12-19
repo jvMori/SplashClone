4124e01f850b1e2f82377b06a3cce57fc277364b
Mori <iwona.moryc92@gmail.com>
2019-12-19 17:05:17 +0100

    [implementation] network bound resource for repository

--- app/src/main/java/com/jvmori/myapplication/core/network/NetworkBoundResource.kt
+++ app/src/main/java/com/jvmori/myapplication/core/network/NetworkBoundResource.kt
@@ -2,35 +2,73 @@ package com.jvmori.myapplication.core.network
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import com.jvmori.myapplication.core.util.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 
-suspend fun<RequestType, ResultType> getData(
+suspend fun <RequestType, ResultType> getData(
     fetchLocalData: suspend () -> Flow<ResultType>,
     fetchNetworkData: suspend () -> Flow<RequestType>,
     refreshNeeded: (ResultType) -> Boolean,
     dataMapper: (RequestType) -> ResultType,
     saveCallResult: suspend (ResultType) -> Unit
-) : LiveData<Result<ResultType>> {
-   return liveData(Dispatchers.IO) {
-       fetchLocalData.invoke().onEach {
-           emit(Result.success(it))
-           if (refreshNeeded(it)){
-               try {
-                   fetchNetworkData.invoke()
-                       .map { data -> dataMapper(data) }
-                       .onEach { networkData ->
-                           saveCallResult(networkData)
-                           emit(Result.success(networkData))
-                       }
-               }catch (e : Exception){
-                   emit(Result.failure(e))
-               }
-           }
-       }
-   }
+): LiveData<Result<ResultType>> {
+    return liveData(Dispatchers.IO) {
+        fetchLocalData.invoke().onEach {
+            emit(Result.success(it))
+            if (refreshNeeded(it)) {
+                try {
+                    fetchNetworkData.invoke()
+                        .map { data -> dataMapper(data) }
+                        .onEach { networkData ->
+                            saveCallResult(networkData)
+                            emit(Result.success(networkData))
+                        }
+                } catch (e: Exception) {
+                    emit(Result.failure(e))
+                }
+            }
+        }
+    }
+}
+
+suspend fun <RequestType, ResultType> fetchData(
+    fetchLocalData: suspend () -> ResultType,
+    fetchNetworkData: suspend () -> RequestType,
+    refreshNeeded: (ResultType) -> Boolean,
+    dataMapper: (RequestType) -> ResultType,
+    saveCallResult: suspend (ResultType) -> Unit
+): Resource<ResultType> {
+    var data: Resource<ResultType> = Resource.loading(null)
+    try {
+        val local = fetchLocalData()
+        data = Resource.success(local)
+        if (refreshNeeded(local)) {
+            data = networkRequest(fetchNetworkData, dataMapper, saveCallResult)
+        }
+    } catch (e: Exception) {
+        networkRequest(fetchNetworkData, dataMapper, saveCallResult)
+    }
+    return data
+}
+
+private suspend fun <RequestType, ResultType> networkRequest(
+    fetchNetworkData: suspend () -> RequestType,
+    dataMapper: (RequestType) -> ResultType,
+    saveCallResult: suspend (ResultType) -> Unit
+): Resource<ResultType> {
+    val data: Resource<ResultType>
+    data = try {
+        val network = fetchNetworkData()
+        val result = dataMapper(network)
+        saveCallResult(result)
+        Resource.success(result)
+    } catch (e: java.lang.Exception) {
+        Resource.error(e.localizedMessage, null)
+    }
+    return data
 }
 
 
