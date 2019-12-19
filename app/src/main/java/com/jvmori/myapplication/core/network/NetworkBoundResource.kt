5260f21f421a2e4baf6a2c3b81db7f3d763a68c9
Mori <iwona.moryc92@gmail.com>
2019-12-19 17:35:54 +0100

    [refactor] deleted unused code in photos repository

--- app/src/main/java/com/jvmori/myapplication/core/network/NetworkBoundResource.kt
+++ app/src/main/java/com/jvmori/myapplication/core/network/NetworkBoundResource.kt
@@ -1,39 +1,8 @@
 package com.jvmori.myapplication.core.network
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
 import com.jvmori.myapplication.core.util.Resource
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
-
-suspend fun <RequestType, ResultType> getData(
-    fetchLocalData: suspend () -> Flow<ResultType>,
-    fetchNetworkData: suspend () -> Flow<RequestType>,
-    refreshNeeded: (ResultType) -> Boolean,
-    dataMapper: (RequestType) -> ResultType,
-    saveCallResult: suspend (ResultType) -> Unit
-): LiveData<Result<ResultType>> {
-    return liveData(Dispatchers.IO) {
-        fetchLocalData.invoke().onEach {
-            emit(Result.success(it))
-            if (refreshNeeded(it)) {
-                try {
-                    fetchNetworkData.invoke()
-                        .map { data -> dataMapper(data) }
-                        .onEach { networkData ->
-                            saveCallResult(networkData)
-                            emit(Result.success(networkData))
-                        }
-                } catch (e: Exception) {
-                    emit(Result.failure(e))
-                }
-            }
-        }
-    }
-}
 
 suspend fun <RequestType, ResultType> fetchData(
     fetchLocalData: suspend () -> ResultType,
