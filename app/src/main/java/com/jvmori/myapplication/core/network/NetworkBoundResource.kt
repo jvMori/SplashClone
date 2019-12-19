fc2e67de7bb1e454961a6041aa523cebad4f0b9e
Mori <iwona.moryc92@gmail.com>
2019-12-19 16:33:06 +0100

    [implementation] network bound resource for repository

--- app/src/main/java/com/jvmori/myapplication/core/network/NetworkBoundResource.kt
+++ app/src/main/java/com/jvmori/myapplication/core/network/NetworkBoundResource.kt
@@ -1,4 +1,36 @@
 package com.jvmori.myapplication.core.network
 
-interface NetworkBoundResource {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+
+suspend fun<RequestType, ResultType> getData(
+    fetchLocalData: suspend () -> Flow<ResultType>,
+    fetchNetworkData: suspend () -> Flow<RequestType>,
+    refreshNeeded: (ResultType) -> Boolean,
+    dataMapper: (RequestType) -> ResultType,
+    saveCallResult: suspend (ResultType) -> Unit
+) : LiveData<Result<ResultType>> {
+   return liveData(Dispatchers.IO) {
+       fetchLocalData.invoke().onEach {
+           emit(Result.success(it))
+           if (refreshNeeded(it)){
+               try {
+                   fetchNetworkData.invoke()
+                       .map { data -> dataMapper(data) }
+                       .onEach { networkData ->
+                           saveCallResult(networkData)
+                           emit(Result.success(networkData))
+                       }
+               }catch (e : Exception){
+                   emit(Result.failure(e))
+               }
+           }
+       }
+   }
 }
+
+
