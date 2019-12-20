c9f2c28ca5b80b165ed1dd8e0ada4c993c8c7347
Mori <iwona.moryc92@gmail.com>
2019-12-20 18:22:17 +0100

    [refactoring] implemented different fragments for each category (photos, collections)

--- app/src/main/java/com/jvmori/myapplication/features/categories/presentation/CategoryPageFragment.kt
+++ app/src/main/java/com/jvmori/myapplication/features/categories/presentation/CategoryPageFragment.kt
@@ -1,80 +1,25 @@
 package com.jvmori.myapplication.features.categories.presentation
 
 
-import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import com.jvmori.myapplication.R
-import com.jvmori.myapplication.core.util.Resource
 import com.jvmori.myapplication.databinding.CategoryPageBinding
-import com.jvmori.myapplication.features.photolist.domain.entities.PhotoEntity
-import com.jvmori.myapplication.features.photolist.presentation.PhotosViewModel
-import org.koin.android.viewmodel.ext.android.viewModel
+import com.jvmori.myapplication.features.CollectionsFragment
+import com.jvmori.myapplication.features.photolist.presentation.PhotosFragment
 
-class CategoryPageFragment : Fragment() {
+open class CategoryPageFragment : Fragment() {
 
-    private val photosViewModel: PhotosViewModel by viewModel(PhotosViewModel::class)
     private lateinit var binding: CategoryPageBinding
     private var id: Int? = -1
 
     companion object {
         fun newInstance(id: Int): CategoryPageFragment {
-            val fragment = CategoryPageFragment()
-            val arg = Bundle()
-            arg.putInt("id", id)
-            fragment.arguments = arg
-            return fragment
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        id = arguments?.getInt("id")
-        when (id){
-            0 -> bindPhotos()
-            1 -> bindCollections()
+            return  when (id){
+                0 -> PhotosFragment()
+                1 -> CollectionsFragment()
+                else -> CategoryPageFragment()
+            }
         }
     }
 
     private fun bindCollections(){}
-
-    private fun bindPhotos() {
-        photosViewModel.fetchPhotos()
-        photosViewModel.photos.observe(this, Observer {
-            when (it.status){
-                Resource.Status.LOADING -> showLoading()
-                Resource.Status.SUCCESS -> showSuccess(it.data)
-                Resource.Status.ERROR -> showError(it.message)
-            }
-        })
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate<CategoryPageBinding>(
-            inflater,
-            R.layout.category_page,
-            container,
-            false
-        ).apply {
-            items = listOf()
-        }
-        return binding.root
-    }
-
-    private fun showLoading(){}
-    private fun showSuccess(data : List<PhotoEntity>?){
-        binding.items = data
-        binding.textView.text = data?.get(0)?.url
-    }
-    private fun showError(errorMessage : String?){
-        Log.i("PHOTOS", errorMessage ?: "")
-    }
 }
