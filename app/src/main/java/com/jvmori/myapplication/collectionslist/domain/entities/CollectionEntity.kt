9fcfbdc688ed049e4ea63e50292a7f736b0c18f4
jvMori <iwona.moryc92@gmail.com>
2020-03-20 18:19:24 +0100

    [implementation] created collection entity data class

---
+++ app/src/main/java/com/jvmori/myapplication/collectionslist/domain/entities/CollectionEntity.kt
@@ -0,0 +1,9 @@
+package com.jvmori.myapplication.collectionslist.domain.entities
+
+data class CollectionEntity(
+    var id: Int,
+    var title: String = "",
+    var totalPhotos: Int = 0,
+    var authorName: String = "",
+    var regularSizeUrl: String = ""
+)
