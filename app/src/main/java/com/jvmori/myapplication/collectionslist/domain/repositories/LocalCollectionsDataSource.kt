6a3e6b8dbe56c67d3e558c2665b9ba2488e407a5
jvMori <iwona.moryc92@gmail.com>
2020-03-27 19:52:43 +0100

    [refactoring]

--- app/src/main/java/com/jvmori/myapplication/collectionslist/domain/repositories/LocalCollectionsDataSource.kt
+++ app/src/main/java/com/jvmori/myapplication/collectionslist/domain/repositories/LocalCollectionsDataSource.kt
@@ -1,9 +1,9 @@
 package com.jvmori.myapplication.collectionslist.domain.repositories
 
 import com.jvmori.myapplication.collectionslist.data.local.CollectionsData
-import com.jvmori.myapplication.common.data.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface LocalCollectionsDataSource {
-    suspend fun getCollections(page: Int): Resource<List<CollectionsData>>
+    suspend fun getCollections(page: Int): Flow<List<CollectionsData>>
     suspend fun insertCollections(data: List<CollectionsData>)
 }
