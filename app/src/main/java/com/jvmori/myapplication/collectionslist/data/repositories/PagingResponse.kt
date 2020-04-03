3f2e1aa27b46171152fbdd9f839f3ce29dc2c268
jvMori <iwona.moryc92@gmail.com>
2020-04-03 20:14:57 +0200

    [refactor] deleted unused class

--- app/src/main/java/com/jvmori/myapplication/collectionslist/data/repositories/PagingResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.jvmori.myapplication.collectionslist.data.repositories
-
-import com.jvmori.myapplication.collectionslist.domain.entities.CollectionEntity
-import com.jvmori.myapplication.common.data.Resource
-
-data class PagingResponse(
-    val data : List<CollectionEntity>,
-    val networkInfo : Resource.Status
-)
