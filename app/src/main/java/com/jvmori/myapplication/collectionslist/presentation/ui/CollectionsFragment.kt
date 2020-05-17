0f10626ee2048faa760ccf71e12494a9a277805c
jvMori <iwona.moryc92@gmail.com>
2020-05-17 20:16:58 +0200

    [implemented] navigation to collection's details from home page

--- app/src/main/java/com/jvmori/myapplication/collectionslist/presentation/ui/CollectionsFragment.kt
+++ app/src/main/java/com/jvmori/myapplication/collectionslist/presentation/ui/CollectionsFragment.kt
@@ -6,14 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.jvmori.myapplication.R
 import com.jvmori.myapplication.collectionslist.presentation.viewmodels.CollectionsViewModel
 import com.jvmori.myapplication.common.data.remote.Resource
+import com.jvmori.myapplication.common.domain.IOnClickListener
 import com.jvmori.myapplication.common.presentation.ui.category.CategoryPageFragment
+import com.jvmori.myapplication.common.presentation.ui.fragments.HomeFragmentDirections
 import com.jvmori.myapplication.databinding.CollectionsFragmentBinding
 import org.koin.android.ext.android.inject
 
-class CollectionsFragment : CategoryPageFragment() {
+class CollectionsFragment : CategoryPageFragment(), IOnClickListener {
 
     private val viewmodel by inject<CollectionsViewModel>()
     private lateinit var collectionsAdapter: CollectionsAdapter
@@ -44,6 +47,12 @@ class CollectionsFragment : CategoryPageFragment() {
         }
     }
 
+    override fun click(position: Int) {
+        val id = collectionsAdapter.getCollection(position)?.id ?: -1
+        val action = HomeFragmentDirections.actionHomeFragment2ToCollectionsPhotos(id)
+        findNavController().navigate(action)
+    }
+
     private fun CollectionsViewModel.observeCollections() {
         collections.observe(this@CollectionsFragment, Observer {
             collectionsAdapter.submitList(it)
@@ -59,6 +68,8 @@ class CollectionsFragment : CategoryPageFragment() {
     }
 
     private fun setupRecyclerView() {
-        collectionsAdapter = CollectionsAdapter.initAdapter(binding.collectionsRv, this.requireContext())
+        collectionsAdapter = CollectionsAdapter.initAdapter(binding.collectionsRv, this.requireContext()).apply {
+            onClickListener = this@CollectionsFragment
+        }
     }
 }
